208c5a70e6de72ac9eed25fc47efd74b6d17c65a
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-03-18 19:11:12 +0100

    Added client javascript and installed socket.io, and initialized socket.io. (can now see the current players)

--- app.js
+++ app.js
@@ -2,17 +2,33 @@
 
 // Packages
 const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
 
 // Default variables
-const port = 5000;
+const port = 4000;
 const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 
 // App settings
-app.use(express.static("public"));
+app.use(express.static("static"));
 
 // App routing
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(port, () => `Listening to port ${port}`);
+// Socket IO
+let players = {};
+
+io.on('connection', socket => {
+  // Add new player to players object
+  players[socket.id] = {};
+
+  // Remove player that left from players object
+  socket.on('disconnect', () => delete players[socket.id]);
+});
+
+server.listen(port, () => console.log(`Listening to port: ${port}`));
