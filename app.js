85c34d5fce2a9df9c2a982ca12aeebce904ccb50
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-03-28 13:21:08 +0100

    Can make the ball move into a random direction at the start

--- app.js
+++ app.js
@@ -24,9 +24,100 @@ app.use(session({secret: "Shh, its a secret!"}));
 // app.use(session({store: sessionStore, secret: "mysecret"}));
 
 const maxPlayers = 2;
+const ballSpeed = 3 / 100; // Is the speed per 1%
 const host = "localhost:4000"; // Might become heroku somthing
+
 let readyPlayers = [];
 let pads = {};
+let ball;
+
+
+class Ball {
+  constructor(x, y) {
+    this.speed;
+    this.yDistance;
+    this.xDistance;
+
+    this.start = {
+      x: x,
+      y: y
+    }
+    this.end = {
+      x: undefined,
+      y: undefined
+    }
+
+    this.startDirection()
+    this.calcDistances()
+    this.calcSpeed()
+  }
+
+  calcDistances() {
+    this.yDistance = Math.abs(this.start.y - this.end.y)
+    this.xDistance = Math.abs(this.start.x - this.end.x)
+  }
+
+  calcSpeed() {
+    const distance = Math.sqrt((this.yDistance * this.yDistance) + (this.xDistance * this.xDistance));
+
+    this.speed = ballSpeed * distance;
+  }
+
+  startDirection() {
+    return new Promise((resolve, reject) => {
+
+
+    const randomBinary = () => Math.round(Math.random()); // 1 or 0
+    const randomBetween = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+    if (randomBinary() === 1) {
+      // Go to the top or bottom first
+
+      console.log("go to the top or bottom")
+      const randomYDirection = randomBinary() ? 100 : 0; // Bottom / Top
+      const randomXDirection = randomBinary();
+      console.log("100 = bottom, 0 = top", randomYDirection)
+      this.end.y = randomYDirection;
+
+      if (randomXDirection === 1) {
+        // Go to right
+        console.log("go the right")
+        this.end.x = randomBetween(75, 90)
+      } else {
+        // Go to left
+        console.log("go the left")
+        this.end.x = randomBetween(10, 25)
+      }
+
+    } else {
+
+      console.log("go to the left or right")
+      const randomXDirection = randomBinary() ? 100 : 0; // right / left
+      const randomYDirection = randomBinary();
+      console.log("100 = right, 0 = left", randomYDirection)
+      this.end.x = randomXDirection;
+
+      if (randomYDirection === 1) {
+        // Go to bottom
+        console.log("go to bottom")
+        this.end.y = randomBetween(50, 90)
+      } else {
+        // Go to top
+        console.log("go to top")
+        this.end.y = randomBetween(10, 50)
+      }
+      // Go to the sides first
+    }
+    })
+  }
+
+}
+
+
+
+
+
+
 
 app.get("/", (req, res) => {
   console.log("in root")
@@ -85,6 +176,8 @@ app.get("/ready", async (req, res) => {
 })
 
 app.get("/gameStart", (req, res) => {
+  ball = new Ball(50, 50)
+  console.log(ball)
   res.render("game.ejs", {
     host: host
   })
@@ -126,6 +219,19 @@ app.post("/pad/update", (req, res) => {
     thisSession: player,
     pads: pads
   })
+})
+
+
+
+app.get("/ball", (req, res) => {
+  res.render("ball.ejs", {ball: ball, host: host})
+})
+
+app.get("/updateBall", async (req, res) => {
+  // ball.update()
+  // await changeDirection();
+  // console.log(ball)
+  // res.redirect("/ball");
 })
 
 function allReady(sessions) {
